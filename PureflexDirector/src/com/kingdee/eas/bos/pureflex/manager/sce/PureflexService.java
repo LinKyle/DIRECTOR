3e3313181e6a8b628f10f421870a75271e474e86
LinKyle <mini_scorpio@sina.com>
2013-09-03 18:00:26 +0800

    workloads

--- PureflexDirector/src/com/kingdee/eas/bos/pureflex/manager/sce/PureflexService.java
+++ PureflexDirector/src/com/kingdee/eas/bos/pureflex/manager/sce/PureflexService.java
@@ -1,5 +1,10 @@
 package com.kingdee.eas.bos.pureflex.manager.sce;
 
+import java.util.List;
+
+import com.kingdee.eas.bos.pureflex.manager.sce.info.WorkLoadInfo;
+
 public interface PureflexService {
 	public boolean loginSceServer(String user,String password) throws Exception;
+	public List<WorkLoadInfo> getWorkLoadList();
 }
